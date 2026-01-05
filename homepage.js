7e6d94ad9990fb8163b2c3bf30ea21435023533c
hannaso91 <hannaso@hiof.no>
2026-01-05 23:39:49 +0100

    fikset slik at bildene kan klikkes på og forstørres

--- homepage.js
+++ homepage.js
@@ -73,3 +73,14 @@ document.addEventListener("DOMContentLoaded", () => {
   setupContactForm();
 });
 
+function openLightbox(img) {
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+
+    lightboxImg.src = img.src;
+    lightbox.style.display = "flex";
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
